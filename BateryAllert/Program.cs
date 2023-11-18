23ce9c3789a1780ed054924bba4b1b9656476d4f
Kronos-XR <nikitka-solova@mail.ru>
2023-11-18 17:13:08 +0300

    Добавьте файлы проекта.

---
+++ BateryAllert/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BateryAllert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((SystemInformation.PowerStatus.BatteryLifePercent * 100).ToString() + "%" , "Заряд батареи %");
+            Console.ReadKey();
+        }
+    }
+}
